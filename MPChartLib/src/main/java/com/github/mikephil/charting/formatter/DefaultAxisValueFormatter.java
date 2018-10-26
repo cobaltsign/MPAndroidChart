7261e9487116f6b5a3dcf93cbbab46a5e60530fc
Cristian Gutu <cristian.gutu@cobaltsign.com>
2018-10-26 11:59:38 +0300

    Revert "Updated the default axis value formatter (try out #1)"
    This reverts commit cf0b58d25f65ab17d2d90db1a0faf3281cf54a4c.

--- MPChartLib/src/main/java/com/github/mikephil/charting/formatter/DefaultAxisValueFormatter.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/formatter/DefaultAxisValueFormatter.java
@@ -30,9 +30,15 @@ public class DefaultAxisValueFormatter implements IAxisValueFormatter
      */
     public DefaultAxisValueFormatter(int digits) {
         this.digits = digits;
-        NumberFormat nf = NumberFormat.getNumberInstance(new Locale("en", "ch"));
-        mFormat = (DecimalFormat)nf;
-        mFormat.setMinimumFractionDigits(digits);
+
+        StringBuffer b = new StringBuffer();
+        for (int i = 0; i < digits; i++) {
+            if (i == 0)
+                b.append(".");
+            b.append("0");
+        }
+
+        mFormat = new DecimalFormat("###,###,###,##0" + b.toString());
     }
 
     @Override
