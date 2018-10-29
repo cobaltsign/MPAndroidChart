23f772d219942b1a2e3f9fc3158d5db4a6cae414
Cristian Gutu <cristian.gutu@cobaltsign.com>
2018-10-29 10:48:43 +0200

    Updated NegativeValueFormatter to use format as the default one and invert just the value.

--- MPChartLib/src/main/java/com/github/mikephil/charting/formatter/NegativeValueFormatter.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/formatter/NegativeValueFormatter.java
@@ -7,10 +7,20 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
 /**
  * Created by alexh for Cobalt Sign SRL on 18-Sep-18.
  */
-public class NegativeValueFormatter implements IValueFormatter,IAxisValueFormatter {
+public class NegativeValueFormatter extends DefaultAxisValueFormatter  implements IValueFormatter {
+    /**
+     * Constructor that specifies to how many digits the value should be
+     * formatted.
+     *
+     * @param digits
+     */
+    public NegativeValueFormatter(int digits) {
+        super(digits);
+    }
+
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
-        return -value + "";
+        return super.getFormattedValue(value * -1, axis);
     }
 
     @Override
